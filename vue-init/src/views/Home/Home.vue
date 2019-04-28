85230a8f6250220204abfeb0cc070153c4c204d5
Ristt <983474780@qq.com>
2019-04-29 00:42:11 +0800

    add

---
+++ vue-init/src/views/Home/Home.vue
@@ -0,0 +1,87 @@
+<template>
+  <el-container>
+    <el-header>
+      <!-- 顶部header -->
+      <el-row :gutter="20" :type="flex">
+        <el-col :span="4">
+          <div class="grid-content">标题</div>
+        </el-col>
+        <el-col :span="16">
+          <div id="header-title-list" class="grid-content">
+            <span></span>
+          </div>
+        </el-col>
+        <el-col :span="4">
+          <div class="grid-content"></div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: white;
+  color: #333;
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  flex: 1;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+.el-row {
+  margin-top: 10px;
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
+
+<script>
+</script>
